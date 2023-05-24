4540e26930437c775c62f6eab95bbce21d0e1d3e
pragati chaudhary <pragatichaudhary23@gmail.com>
2023-05-24 18:04:11 +0545

    let's push the code

---
+++ Routes/tree.js
@@ -0,0 +1,2 @@
+const router = require("express");
+const app = router();
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/user1", (req, res) => {
+  res.send("Hello pragati");
+});
+
+app.get("/user2", (req, res) => {
+  res.json({ username: "Hello shivani", age: "22", city: "Heart" });
+});
+
+app.get("/user3", (req, res) => {
+  res.send("Hello ritu");
+});
+
+app.listen(3000, () => console.log("The application is running on port 3000"));
+module.exports = express;
